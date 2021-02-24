f0eab9a5f41f6921eb379f5321e734ebe333bd3b
martin kuracka <m.kuracka@gmail.com>
2021-02-24 10:26:49 +0100

    finished styling

---
+++ script.js
@@ -0,0 +1,24 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to media element and play
+async function selectStreamToDisplay() {
+    try {        
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+        videoElement.requestPictureInPicture();
+        videoElement.play();
+        }
+    } catch(error) {
+        console.log('SOMETHING WENT WRONG:', error)
+    }
+};
+
+//  event listener to button press
+button.addEventListener('click',() => {
+    selectStreamToDisplay();
+    
+} )
+// On load
+// selectStreamToDisplay();
